d1383d156c80e907cd8fd019617f2f89673925a0
Nikita Belousov <belousovn14@gmail.com>
2022-12-11 12:38:16 +0300

    - Рефокторинг класса базы данных

--- database/db_function_connect.py
+++ database/db_function_connect.py
@@ -1,152 +1,131 @@
 import sqlite3
 import hashlib
-class dbConnect:
 
-    def connectionOpen():
-        connection = sqlite3.connect('db.sqlite3')  # подключение к бд
-        cursor = connection.cursor()
-        return (connection, cursor)
+class DataBase:
 
+    def __init__(self, dirDataBase):
+        self.connection, self.cursor = self.connection(dirDataBase)
 
-    def connectionClose(connection, cursor):
-        cursor.close()
-        connection.close()
+    def connectionOpen(self, dir='db.sqlite3'):
+        try:
+            connection = sqlite3.connect(dir)  # подключение к бд
+            cursor = connection.cursor()
 
+            return connection, cursor
+
+        except:
+            print("Не удалось подключиться к БД")
+
+    def connectionClose(self):
+        self.cursor.close()
+        self.connection.close()
 
     def array_creator(self, array_of_values):  # функция перегона кортежа в массив 
         temp_storage = array_of_values
         array_of_values = []
         for value in temp_storage:
             array_of_values += [value[0]]
+
         return array_of_values
 
-
     def getCarsNumbers(self):  # функция для вывода всех автомобильных номеров
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT NUMBER FROM CAR')
+        self.cursor.execute('SELECT NUMBER FROM CAR')
         cars_numbers = cursor.fetchall()
         cars_numbers = array_creator(cars_numbers)
         
         return cars_numbers  # выводится из бд в обратном порядке, от последних к первым
 
-
-    def getDataOfCar(self,car_number):  # вывод сведений об автомобиле по его номеру  
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT CAR.PHOTO, PEOPLES.NAME FROM CAR JOIN PEOPLES ON CAR.ID_OWNER = PEOPLES.ID WHERE NUMBER=?', (car_number, ))
+    def getDataOfCar(self, car_number):  # вывод сведений об автомобиле по его номеру
+        self.cursor.execute('SELECT CAR.PHOTO, PEOPLES.NAME FROM CAR JOIN PEOPLES ON CAR.ID_OWNER = PEOPLES.ID WHERE NUMBER=?', (car_number, ))
         car_data = cursor.fetchone()
-        dbConnect.connectionClose(connection, cursor)
         return car_data  # данные в форме кортежа (путь_к_фото_авто, ФИО)
 
-
-    def getDataOfPeople(self,people_id): # вывод сведений о человеке по его id
-        connection, cursor = dbConnect.connectionOpen()
-        people_data = cursor.execute('SELECT PHOTO, NAME, STATUS FROM PEOPLES WHERE ID=?', (people_id, ))
-        dbConnect.connectionClose(connection, cursor)
+    # вывод сведений о человеке по его id
+    def getDataOfPeople(self, people_id):
+        people_data = self.cursor.execute('SELECT PHOTO, NAME, STATUS FROM PEOPLES WHERE ID=?', (people_id, ))
         return people_data  # данные в форме кортежа (путь_к_фото_человека, ФИО, должность)
 
 
     def addVisit(self,status_car, people_id, datetime): # функция добавления посещений
-        connection, cursor = dbConnect.connectionOpen()
         cursor.execute('INSERT INTO VISITS (ID_PEOPLE, IS_CAR, DATETIME) VALUES (?, ?, ?)', (people_id, status_car, datetime, ))
-        connection.commit()
-        dbConnect.connectionClose(connection, cursor)
-
+        self.connection.commit()
 
     def getAllVisits(self):  # функция для вывода всех посещений как классов
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT PEOPLES.NAME, VISITS.DATETIME, VISITS.IS_CAR FROM VISITS JOIN PEOPLES ON VISITS.ID_PEOPLE=PEOPLES.ID ORDER BY DATETIME')
-        visits = cursor.fetchall()
-        dbConnect.connectionClose(connection, cursor)
-        class visit:
-            def __init__(self, name, date_time, is_car):
-                self.name = name
-                self.date = date_time
-                self.is_car = is_car
-
-            def name(self):
-                return self.name
-
-            def date_time(self):
-                return self.date_time
-
-            def is_car(self):
-                return self.is_car
+        self.cursor.execute('SELECT PEOPLES.NAME, VISITS.DATETIME, VISITS.IS_CAR FROM VISITS JOIN PEOPLES ON VISITS.ID_PEOPLE=PEOPLES.ID ORDER BY DATETIME')
+        visits = self.cursor.fetchall()
 
         visits_objects_list = []
         for vis in visits:
-            vis = visit(vis[0],vis[1],vis[2])
+            vis = visit(vis[0], vis[1], vis[2])
             visits_objects_list += [vis]
         return visits_objects_list
 
-
-
-    def is_authenticate(login, password):  # функция для проверки правильности аутентификации
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT PASSWORD FROM SECURITIES WHERE LOGIN=?', (login, ))
+    def is_authenticate(self, login, password):  # функция для проверки правильности аутентификации
+        self.cursor.execute('SELECT PASSWORD FROM SECURITIES WHERE LOGIN=?', (login, ))
         password_db = cursor.fetchall()[0][0]
-        dbConnect.connectionClose(connection, cursor)
         # сюда тоже вставить хешер
         if password_db == password:
-            return('TRUE')
+            return True
         else:
-            return('FALSE')
+            return False
 
-
-
-    def camera_select(security_id):  # функция для выборки id камер по id охранника (сначала камера ворот, потом- шлагбаума), можно обратиться по индексам 0 и 1 соответственно
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT ID_GATE_CAMERA, ID_BARRIER_CAMERA  FROM SECURITIES WHERE ID=?', (security_id, ))
-        cameras = cursor.fetchall()
-        dbConnect.connectionClose(connection, cursor)
+    def camera_select(self, security_id):  # функция для выборки id камер по id охранника (сначала камера ворот, потом- шлагбаума), можно обратиться по индексам 0 и 1 соответственно
+        self.cursor.execute('SELECT ID_GATE_CAMERA, ID_BARRIER_CAMERA  FROM SECURITIES WHERE ID=?', (security_id, ))
+        cameras = self.cursor.fetchall()
         return cameras
 
 
-    def addSecurity(lastname, firstname, patronym, login, password, id_gate_camera, id_barrier_camera):  # какой-то ужасный запрос получается
-        connection, cursor = dbConnect.connectionOpen()
+    def addSecurity(self, lastname, firstname, patronym, login, password, id_gate_camera, id_barrier_camera):  # какой-то ужасный запрос получается
         password = password.encode()
         password = hashlib.md5(password)
         password = password.hexdigest()
         print(password)
 
-        cursor.execute('INSERT INTO SECURITIES (LASTNAME, FIRSTNAME, PATHRONYM, LOGIN, PASSWORD, ID_GATE_CAMERA, ID_BARRIER_CAMERA) VALUES (?, ?, ?, ?, ?, ?, ?)', (lastname, firstname, patronym, login, password, id_gate_camera, id_barrier_camera, ))
-        connection.commit()
-        dbConnect.connectionClose(connection, cursor)
-        
+        self.cursor.execute('INSERT INTO SECURITIES (LASTNAME, FIRSTNAME, PATHRONYM, LOGIN, PASSWORD, ID_GATE_CAMERA, ID_BARRIER_CAMERA) VALUES (?, ?, ?, ?, ?, ?, ?)', (lastname, firstname, patronym, login, password, id_gate_camera, id_barrier_camera, ))
+        self.connection.commit()
 
-
-    def editPassword(login, password, nev_password):  # функция для замены пароля охранника
-        if len(passvord) < 6:
-            return('FALSE')
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT PASSWORD FROM SECURITIES WHERE LOGIN=?', (login, ))
-        password_db = cursor.fetchall()[0][0]
+    def editPassword(self, login, password, new_password):  # функция для замены пароля охранника
+        if len(new_password) < 6:
+            return False
+        self.cursor.execute('SELECT PASSWORD FROM SECURITIES WHERE LOGIN=?', (login, ))
+        password_db = self.cursor.fetchall()[0][0]
         password = password.encode()
         password = hashlib.md5(password)
         password = password.hexdigest()
         print(password)
         print(password_db)
         if password_db == password:
-            nev_password = nev_password.encode()
-            nev_password = hashlib.md5(nev_password)
-            nev_password = nev_password.hexdigest()
-            print(nev_password)
-            cursor.execute('UPDATE SECURITIES SET PASSWORD=? WHERE LOGIN=?', (nev_password, login, ))
+            new_password = new_password.encode()
+            new_password = hashlib.md5(new_password)
+            new_password = new_password.hexdigest()
+            print(new_password)
+            cursor.execute('UPDATE SECURITIES SET PASSWORD=? WHERE LOGIN=?', (new_password, login, ))
             connection.commit()
-            dbConnect.connectionClose(connection, cursor)
             # заменить в бд запись с паролем на новую
         else:
-            dbConnect.connectionClose(connection, cursor)
-            return('FALSE')
+            return False
 
-    def accessAttempts(computer_id, datetime):  # функция для записи неужачных попыток входа
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('INSERT INTO access_attempts (computer_id, datetime) VALUES (?, ?)', (computer_id, datetime, ))
-        connection.commit()
-        dbConnect.connectionClose(connection, cursor)
+    def accessAttempts(self, computer_id, datetime):  # функция для записи неужачных попыток входа
+        self.cursor.execute('INSERT INTO access_attempts (computer_id, datetime) VALUES (?, ?)', (computer_id, datetime, ))
+        self.connection.commit()
 
-    def getAccessAttempts(): # функция для вывода всех неудачных попыток входа в систему
-        connection, cursor = dbConnect.connectionOpen()
-        cursor.execute('SELECT computer_id, datetime FROM access_attempts ORDER BY ')
-dbConnect.editPassword('ivanov', '1', '2')
+    # функция для вывода всех неудачных попыток входа в систему
+    def getAccessAttempts(self):
+        self.cursor.execute('SELECT computer_id, datetime FROM access_attempts ORDER BY ')
+        self.editPassword('ivanov', '1', '2')
 
+class visit:
+    def __init__(self, name, date_time, is_car):
+        self.name = str(name)
+        self.date = str(date_time)
+        self.is_car = str(is_car)
 
+    # гетеры
+    def getName(self):
+        return self.name
+
+    def getDateTime(self):
+        return self.date_time
+
+    def getCar(self):
+        return self.is_car
